8676cfac05a85c74c17926e1a8dec67f32dacf33
anosidium <39738481+anosidium@users.noreply.github.com>
2018-11-16 18:56:21 +0000

    My solution to the challenge

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const fibonacci = (n) => {
 	Once it works the tests will pass
 
 	*/
-	return n
+	
+	let fibonacciSequence = [0, 1, 1, 2];
+
+	if (n > 3)
+		for (let i = 4; i <= n; i++)
+			fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2]);
+
+	return fibonacciSequence[n];
 }
 
 
@@ -29,7 +36,23 @@ const flatten = (list) => {
 
 	*/
 
-	return list
+	let flattenArray = [];
+
+	for (let i = 0; i < list.length; i++) {
+		if (list[i].length > 0) {
+			for (let j = 0; j < list[i].length; j++) {
+				if (list[i][j].length > 0)
+					for (let k = 0; k < list[i][j].length; k++)
+						flattenArray.push(list[i][j][k]);
+				else
+					flattenArray.push(list[i][j]);
+			}
+		} else {
+			flattenArray.push(list[i]);
+		}
+	}
+
+	return flattenArray;
 }
 
 
@@ -48,7 +71,45 @@ const countLetters = (sequence) => {
 	Once it works the tests will pass
 	*/
 
-	return counts
+	let alphabets = [["A", 0],
+					 ["B", 0],
+					 ["C", 0],
+					 ["D", 0],
+					 ["E", 0],
+					 ["F", 0],
+					 ["G", 0],
+					 ["H", 0],
+					 ["I", 0],
+					 ["J", 0],
+					 ["K", 0],
+					 ["L", 0],
+					 ["M", 0],
+					 ["N", 0],
+					 ["O", 0],
+					 ["P", 0],
+					 ["Q", 0],
+					 ["R", 0],
+					 ["S", 0],
+					 ["T", 0],
+					 ["U", 0],
+					 ["V", 0],
+					 ["W", 0],
+					 ["X", 0],
+					 ["Y", 0],
+					 ["Z", 0]];
+
+	let letters = sequence.split("");
+
+	for (let i = 0; i < alphabets.length; i++)
+		for (let j = 0; j < letters.length; j++)
+			if (letters[j] == alphabets[i][0])
+				alphabets[i][1] += 1;
+
+	for (let i = 0; i < alphabets.length; i++)
+		if (alphabets[i][1] > 0)
+			counts[alphabets[i][0]] = alphabets[i][1];
+
+	return counts;
 }
 
 
